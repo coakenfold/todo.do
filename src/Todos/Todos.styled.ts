c29d096ed0fbe1514c15b91522a5f5d06211906c
Chad Oakenfold <chad@oakenfold.ca>
2022-09-01 20:12:41 -0400

    More mobile styling

--- src/Todos/Todos.styled.ts
+++ src/Todos/Todos.styled.ts
@@ -3,13 +3,16 @@ import styled from "styled-components";
 export const TodoGroup = styled.div`
   font-family: sans-serif;
   background-color: #fff;
-  border-radius: 1rem;
   padding: 1rem;
   margin: 0 auto;
-  box-shadow: 0px 10px 40px 0px rgb(76 70 124 / 50%);
 
   max-width: 66rem;
   min-width: 35rem;
+
+  @media (min-width: 600px) {
+    border-radius: 1rem;
+    box-shadow: 0px 10px 40px 0px rgb(76 70 124 / 50%);
+  }
 `;
 
 export const TodoTitle = styled.h1`
